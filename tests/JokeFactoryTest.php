14d4fcfd8218535302bb6a62ddd282c134619646
R. Kukuh <rkukuh@gmail.com>
2020-02-04 06:19:41 +0700

    Styling

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,10 +3,10 @@
 namespace Rkukuh\OneLineJokes\Tests;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Handler\MockHandler;
 use Rkukuh\OneLineJokes\JokeFactory;
 
 class JokeFactoryTest extends TestCase
